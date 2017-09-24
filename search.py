0176b2968ccfab2c2eec81712a9d066ab9b56e45
Justin Pugliese <jmpuglie@us.ibm.com>
2017-09-24 16:37:44 -0400

    makes requests and asks for manual feedback on first round of results

--- search.py
+++ search.py
@@ -1,9 +1,14 @@
 import sys
+import requests
+
+from string import Template
 
 CLIENT_KEY = ""
 ENGINE_KEY = ""
 PRECISION = 1.0
 QUERY = ""
+
+URL = Template("https://www.googleapis.com/customsearch/v1?key=$client_key&cx=$engine_key&q=$query")
 
 def print_parameters():
     print "Parameters:"
@@ -12,6 +17,34 @@ def print_parameters():
     print "Query      = " + QUERY
     print "Precision  = %.1f" % (PRECISION)
 
+def print_results_header():
+    print "Google Search Results:"
+    print "======================"
+
+def print_result(item):
+    print "["
+    print " URL: " + item["link"]
+    print " Title: " + item["title"]
+    print " Summary: " + item["snippet"]
+    print "]"
+
+def query():
+    """Send request to Google Custom Search endpoint."""
+    url = URL.substitute(client_key = CLIENT_KEY, engine_key = ENGINE_KEY, query = QUERY)
+    print "---"
+    print url
+    print "---"
+    response = requests.get(url)
+    items = response.json()["items"]
+    check_relevance(items)
+
+def check_relevance(items):
+    """Loop through results and ask for manual feedback."""
+    print_results_header()
+    for index in range(len(items)):
+        print "Result %d" % (index + 1)
+        print_result(items[index])
+        items[index]["relevance"] = raw_input("Relevant (Y/N)?")
 
 def main():
     """Main entry point for the script."""
@@ -25,9 +58,10 @@ def main():
     PRECISION = float(sys.argv[3])
 
     global QUERY
-    QUERY = sys.argv[4]
+    QUERY = "per se" #sys.argv[4]
 
     print_parameters()
+    query()
 
 if __name__ == '__main__':
     sys.exit(main())
