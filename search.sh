b1f9b1eb062b39c08b4a10836bcab9ba435fba1c
Alexander Castleton <awcastleton@gmail.com>
2017-10-04 23:06:40 -0400

    testing

--- search.sh
+++ search.sh
@@ -9,5 +9,5 @@ fi
 
 if [$# -eq 3 ]
     then
-        /usr/bin/python ./search.py AIzaSyCATX_cG2DgsJjFtCdgcThfR2xaH7MSMl0 010829534362544137563:ndji7c0ivva $1 "$2" $3
+        /usr/bin/python ./search.py AIzaSyCATX_cG2DgsJjFtCdgcThfR2xaH7MSMl0 010829534362544137563:ndji7c0ivva $1 "$2" "$3"
 fi
